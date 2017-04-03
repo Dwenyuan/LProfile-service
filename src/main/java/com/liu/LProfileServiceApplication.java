0a6c0441f46b5ca4f69df89e1bc24f41c4e93775
liu <liu@DESKTOP-CLCFGAG.lan>
2017-04-04 01:46:20 +0800

    初始化提交

---
+++ src/main/java/com/liu/LProfileServiceApplication.java
@@ -0,0 +1,12 @@
+package com.liu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LProfileServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LProfileServiceApplication.class, args);
+	}
+}
